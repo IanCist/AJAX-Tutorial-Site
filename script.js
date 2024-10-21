be1cd5573ceb57d1372e1e76235a89782820028f
IanCist <iantcisternas@gmail.com>
2024-10-21 11:52:38 -0400

    working on script

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-var myCat = {
-    "name": "Meowsalot",
-    "species": "cat",
-    "favFood": "Tuna",
-}
+var btn = document.getElementById("btn");
 
-myCat.favFood
+btn.addEventListener("click", function() {
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+};
+ourRequest.send();
+});
+
+function renderHTML(data) {
+
+}
