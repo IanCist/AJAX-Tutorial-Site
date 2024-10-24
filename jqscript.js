236f851add492e03175bdde0d9bdb88c0633e2fc
IanCist <iantcisternas@gmail.com>
2024-10-24 12:03:49 -0400

    fixed issue

--- jqscript.js
+++ jqscript.js
@@ -4,34 +4,33 @@ const btn = $('#btn');
 
 btn.on('click', function () {
     $.ajax({
-        url: 'https://learnwebcode.github.io/json-example/animals-${pageCounter}.json',
+        url: `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`, 
         method: 'GET',
         success: function (data) {
-            renderHTML(data)
-            pageCounter++
+            renderHTML(data);
+            pageCounter++;
             if (pageCounter > 3) {
                 btn.addClass("hide-me");
             }
         },
-        error: function(){
-            console.log('Connection error')
+        error: function() {
+            console.log('Connection error');
         }
-    })
-})
+    });
+});
 
 function renderHTML(data) {
-    let htmlString = ''
-    data.forEach(function(animal){
-        htmlString += `<p>${animal.name} is a ${animal.species} that likes to eat`
-        animal.foods.likes.forEach(function(like, index){
-            htmlString += index === 0 ? like : ` and ${like}`
-        })
-        htmlString += ' and dislikes'
-        animal.foods.dislikes.forEach(function(dislike, index){
-            htmlString += index === 0 ? dislike : ` and ${dislike}`
-    })
-    animalContainer.append(htmlString)
+    let htmlString = '';
+    data.forEach(function(animal) {
+        htmlString += `<p>${animal.name} is a ${animal.species} that likes to eat `;
+        animal.foods.likes.forEach(function(like, index) {
+            htmlString += index === 0 ? like : ` and ${like}`;
+        });
+        htmlString += ' and dislikes ';
+        animal.foods.dislikes.forEach(function(dislike, index) {
+            htmlString += index === 0 ? dislike : ` and ${dislike}`;
+        });
+        htmlString += '.</p>'; 
+    });
+    animalContainer.append(htmlString); 
 }
-
-
-
